07e2b1625593cdb34d52f0352fa2ce2705532c7e
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-11-26 19:40:05 +0900

    トップページ雛形完成

--- app/javascript/top_page.js
+++ app/javascript/top_page.js
@@ -1,6 +1,6 @@
 $(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 700) {
+    if ($(this).scrollTop() > 650) {
       $('#menu').addClass('fixed');
     } else {
       $('#menu').removeClass('fixed');
