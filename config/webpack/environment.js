c0947392d28eccde45d7a4916203845279c7287d
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-12-15 09:45:30 +0900

    修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,6 +5,5 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery'
-    Popper: ['popper.js', 'default']
   }))
 module.exports = environment
