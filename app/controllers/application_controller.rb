3b99b75d465034e1165d3a758af9b8d5a8b48cec
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-12-15 09:37:47 +0900

    test デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     stocks_path
   end
+
+  def after_sign_out_path_for(resource)
+    stocks_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'stocks/index'
   devise_for :users
   root to: "portfolios#index"
   resources :stocks
